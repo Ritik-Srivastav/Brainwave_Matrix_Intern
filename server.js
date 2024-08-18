504d78a0a9fe03451f785e2a219bf425dd0cfd8f
Ritik Srivastav <120705024+Ritik-Srivastav@users.noreply.github.com>
2024-08-18 16:57:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = 5001;
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URL)
